1c3bc34c5fd09606d86690293534f111eb35e691
SimonBFornillos <simonbfornillos@gmail.com>
2023-09-15 18:19:46 +0800

    added local API

---
+++ src/content.js
@@ -0,0 +1,43 @@
+const logotext = "GRAZIE 1322"
+const meta = {
+    title: "GRAZIE 1322",
+};
+
+const introdata = {
+    title: "Welcome to Grazie 1322",
+    animated: {
+        first: "Where Curiosity,",
+        second: "Comfort",
+        third: "and Goodness Unite!",
+    },
+};
+
+const missiondata = {
+    title: "Mission",
+    missiondesc: "At Grazie, our mission is to cultivate a lifestyle of balance, curiosity, and gratitude in the digital age. Through thoughtfully curated products, we inspire and support good physical and digital reading habits, foster healthy eating choices, and encourage a mindset of constant appreciation. Through our offerings, we aim to guide individuals towards a harmonious existence that embraces both the virtual and the tangible.",
+};
+
+const visiondata = {
+    title: "Vision",
+    visiondesc: "Our vision is to be a guiding light in the modern world, where reading enriches minds, nourishment fuels vitality, and gratitude permeates every moment. Grazie envisions a future where our products empower individuals to navigate the digital landscape while staying grounded in meaningful experiences. We strive to create a community that values learning, well-being, and thankfulness, enhancing lives even amidst the digital era's rapid pace.",
+};
+
+const aboutdata = {
+    title: "Who are we?",
+    aboutdesc: "At Grazie, we're in the business of creating smiles and connections. We're a team of dreamers and doers, united by a love for learning, well-being, and givers of gratitude. We've curated an online haven where cozy corners meet cutting-edge learning, where nourishment extends beyond the plate, and where every product holds the magic of shared moments and every connection sparks a smile.",
+}
+
+const contactdata = {
+    email: "grazie1322onlinestore@gmail.com",
+    description: "Have a question, an idea, or just want to share a virtual high-five? Reach out to us at our email or drop us a line via our contact form. We can't wait to connect!"
+}
+
+export {
+    logotext,
+    meta,
+    introdata,
+    missiondata,
+    visiondata,
+    aboutdata,
+    contactdata,
+}
